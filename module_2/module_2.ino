08a626a8ba46eb31a20a99bf9e589245b3a918f9
Mauro Cifuentes <41113711+maurocifuentes@users.noreply.github.com>
2022-09-10 18:26:30 -0300

    press / release actions - debug - WIP

--- module_2/module_2.ino
+++ module_2/module_2.ino
@@ -1,12 +1,12 @@
 #include "Arduino.h"
-#include "DFRobotDFPlayerMini.h"
+//#include "DFRobotDFPlayerMini.h"
 
 /* 
   Modulo 2: Intercom y reproductor de musica (ARDUINO MEGA con 2 reproductores MP3)
   Intercom
   --------
     1.- al pulsar el boton del intercom suena un pitido, al soltar reproduce un audio (respuesta) al azar
-    2. Organizacion de la SD: carpeta 001 tiene el pitido, a partir de alli vamos a poner n respuestas fijas (que van de 002 a 0nn) 
+    2. Organizacion de la SD: carpeta 001 tiene el pitido, a partir de alli vamos a poner n respuestas fijas (que van de 003 a 013) 
        que son las que se reproduciran al azar
 
   Reproductor de musica
@@ -17,11 +17,11 @@
        (se diferencia del anterior en que en el anterior reproduce al soltar, aqui al terminar)  
  
     4. Al retirar la tarjeta (soltar el switch) frena la reproduccion y emite un pitido (diferente al anterior) 
-    4. Organizacion de la SD: 001 y 002 : pitidos, 003 en adelante: archivos de musica
+    4. Organizacion de la SD: 001 y 002 : pitidos, 023 a 033 en adelante: archivos de musica
 */
 
-DFRobotDFPlayerMini intercomPlayer;
-DFRobotDFPlayerMini musicPlayer;
+//DFRobotDFPlayerMini intercomPlayer;
+//DFRobotDFPlayerMini musicPlayer;
 
 byte isPlayingIntercomPlayer = 0;
 byte isPlayingMusicPlayer = 0;
@@ -31,20 +31,68 @@ byte isPlayingMusicPlayer = 0;
 // filename: 001.mp3 (released action)
 //           002.mp3 (press action)
 
-void playSoundPress(byte keyPressed) {
-  //myDFPlayer.playFolder(keyPressed + 1, 2);
-  isPlaying = keyPressed + 1;  
+
+void playSoundPress(byte lastKeyPressed) {
+  if(lastKeyPressed == 0) {
+    // intercom
+    playIntercomPress();
+  } else {
+    playMusicPress();
+  }  
 }
 
-void playSoundReleased(byte keyPressed) {
-  if(isPlaying != 0) {
-    //myDFPlayer.stop();
-    isPlaying = 0;
+
+void playSoundReleased(byte lastKeyPressed) {
+  if(lastKeyPressed == 0) {
+    // Intercom
+    playIntercomReleased();
   } else {
-    //myDFPlayer.playFolder(keyPressed + 1, 1);
-    isPlaying = keyPressed + 1;
-  }
+    // Music Player
+    playMusicReleased();
+  }    
 }
+
+
+void playIntercomPress() {
+  //intercomPlayer.playFolder(keyPressed + 1, 2);
+  isPlayingIntercomPlayer = 1;
+
+  Serial.println("Pitido inicial");
+}
+
+void playIntercomReleased() {
+  // if(isPlayingIntercomPlayer != 0) {
+    //intercomPlayer.stop();
+    // isPlayingIntercomPlayer = 0;
+//  } else {
+    //intercomPlayer.playFolder(keyPressed + 1, 1);
+    isPlayingIntercomPlayer = 1;
+//  }
+  Serial.println("Tocando respuesta al azar");
+}
+
+
+void playMusicPress() {
+  //musicPlayer.playFolder(keyPressed + 1, 2);
+  isPlayingMusicPlayer = 1;
+
+  Serial.println("Toca pitido");
+  Serial.println("Al finalizar toca una cancion al azar");
+  
+}
+
+void playMusicReleased() {
+//  if(isPlayingMusicPlayer != 0) {
+    //musicPlayer.stop();
+//    isPlayingMusicPlayer = 0;
+//  } else {
+    //musicPlayer.playFolder(keyPressed + 1, 1);
+    isPlayingMusicPlayer = 1;
+//  }
+    Serial.println("Frena el reproductor si esta tocando algo");
+    Serial.println("Toca Pitido de salida");
+}
+
 
 void setup()
 {
@@ -53,7 +101,7 @@ void setup()
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial2.begin(9600);  
-
+/*
   intercomPlayer.begin(Serial1);
   intercomPlayer.EQ(DFPLAYER_EQ_NORMAL);
   intercomPlayer.setTimeOut(500);
@@ -65,10 +113,12 @@ void setup()
   musicPlayer.setTimeOut(500);
   musicPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   musicPlayer.volume(20);  //Set volume value. From 0 to 30
+*/  
 }
 
 void loop()
 {
+/*  
   if(intercomPlayer.available()) {
     // triggered on finish play
     if(intercomPlayer.readType() == 5) {
@@ -82,6 +132,6 @@ void loop()
       isPlayingMusicPlayer = 0;
     }
   }
-  
+*/
   loopSwitches();
 }
